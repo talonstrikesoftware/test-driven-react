afb5ef9ff0650eb2cb43db5e0f4c4a7e1b52423a
Mark <redwolf89@gmail.com>
2020-03-21 10:34:00 -0400

    Made HasIndex use controllable pattern - commit

--- project/test-driven-carousel/src/tests/Carousel.test.js
+++ project/test-driven-carousel/src/tests/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Carousel, { Carousel as CoreCarousel } from '../Carousel';
 import CarouselButton from '../CarouselButton';
 import CarouselSlide from '../CarouselSlide';
@@ -136,6 +136,12 @@ describe('Carousel', () => {
       expect(wrapper.find(CoreCarousel).prop('slides')).toBe(slides);
     });
 
+    it ('allows `slideIndex` to be controlled', () => {
+      const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(1);
+      mounted.setProps({ slideIndex: 0});
+      expect(mounted.find(CoreCarousel).prop('slideIndex')).toBe(0);
+    });
   });
 
   describe('core component', () => {
